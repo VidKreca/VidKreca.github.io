8f499e7ea681576ea911799eaefd5f4b7849ff79
Vid Kreča <34378985+VidKreca@users.noreply.github.com>
2022-05-27 15:15:27 +0200

    Added terminal button and files

---
+++ js/console.js
@@ -0,0 +1,3 @@
+const input = document.querySelector('input[type="text"]');
+const prepend = document.querySelector("#input-prepend");
+const commandsContainer = document.querySelector("#commands");
